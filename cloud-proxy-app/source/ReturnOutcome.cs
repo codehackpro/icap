d921565988757384984285fdbd734d32681bbe7e
Jameel Nabbo <jameelnabbo@gmail.com>
2020-10-28 11:11:45 +0200

    Update ReturnOutcome.cs

--- cloud-proxy-app/source/ReturnOutcome.cs
+++ cloud-proxy-app/source/ReturnOutcome.cs
@@ -1,5 +1,7 @@
 ﻿namespace Glasswall.IcapServer.CloudProxyApp
 {
+    //string password ="1233456";
+    
     enum ReturnOutcome
     { 
         GW_UNPROCESSED = 0, GW_REBUILT = 1, GW_FAILED = 2, GW_ERROR = 9 
